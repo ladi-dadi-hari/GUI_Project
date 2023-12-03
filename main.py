02ab2931c3545b7b55e269b8f7923c6a6647e2f0
Harris <harris.nuhanovic@stud.fra-uas.de>
2023-12-03 21:32:48 +0100

    frames erstellt und basic functionalities

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import time
+from tkinter import ttk
 
 def show_frame(frame):
     frame.tkraise()
@@ -8,24 +8,66 @@ root = tk.Tk()
 root.geometry('1200x900')  # Setzt die Größe des Fensters auf 1200x900
 root.configure(bg='white')  # Setzt die Hintergrundfarbe auf Weiß für den hellen Modus
 
-# Willkommensbildschirm
-welcome_frame = tk.Frame(root, bg='white')
-welcome_frame.grid(row=0, column=0, sticky='news')
-welcome_label = tk.Label(welcome_frame, text="3D-Visualisierung", bg='white')
-welcome_label.pack(pady=450)  # Zentriert den Text vertikal
-
 # Hauptframe
 main_frame = tk.Frame(root, bg='white')
-main_frame.grid(row=0, column=0, sticky='news')
-button_3d = tk.Button(main_frame, text="3D-Visualisierung", command=lambda: None, bg='white')  # Fügen Sie Ihre Funktion hier ein
+main_frame.place(relwidth=1, relheight=1)
+
+# 3D-Visualisierungs-Frame
+frame_3d = tk.Frame(root, bg='white')
+frame_3d.place(relwidth=1, relheight=1)
+
+# 2D-Koordinatensystem-Frame
+frame_2d = tk.Frame(root, bg='white')
+frame_2d.place(relwidth=1, relheight=1)
+
+# 2D-Koordinatensysteme-Frame
+frame_2d_systems = tk.Frame(root, bg='white')
+frame_2d_systems.place(relwidth=1, relheight=1)
+
+# Button-Frame im Hauptframe
+button_frame = tk.Frame(main_frame, bg='white')
+button_frame.place(relx=0.5, rely=0.5, anchor='center')
+
+button_3d = tk.Button(button_frame, text="3D-Visualisierung", command=lambda: show_frame(frame_3d), bg='white', fg='black')
 button_3d.pack(pady=10)  # Zentriert den Button vertikal und fügt einen Abstand zwischen den Buttons hinzu
-button_2d = tk.Button(main_frame, text="2D-Koordinatensystem", command=lambda: None, bg='white')  # Fügen Sie Ihre Funktion hier ein
+
+button_2d = tk.Button(button_frame, text="2D-Koordinatensystem", command=lambda: show_frame(frame_2d), bg='white', fg='black')
 button_2d.pack(pady=10)  # Zentriert den Button vertikal und fügt einen Abstand zwischen den Buttons hinzu
 
-# Zeige zuerst den Willkommensbildschirm
-show_frame(welcome_frame)
+# Fügen Sie hier den Inhalt für jeden Frame hinzu
+#label_3d = tk.Label(frame_3d, text="3D-Visualisierung")
+#label_3d.pack()
 
-# Wechsle nach 3 Sekunden zum Hauptframe
-root.after(3000, show_frame, main_frame)
+#label_2d = tk.Label(frame_2d, text="2D-Koordinatensystem")
+#label_2d.pack()
+
+# Zurück-Buttons für die Frames frame_3d und frame_2d
+back_button_3d = tk.Button(frame_3d, text="Zurück", command=lambda: show_frame(main_frame), bg='white', fg='black')
+back_button_3d.pack(anchor='nw')  # Positioniert den Button in der linken oberen Ecke
+
+back_button_2d = tk.Button(frame_2d, text="Zurück", command=lambda: show_frame(main_frame), bg='white', fg='black')
+back_button_2d.pack(anchor='nw')  # Positioniert den Button in der linken oberen Ecke
+
+# Combobox für den Zeitraum der Datenauswertung
+label_time = tk.Label(frame_2d, text="Zeitraum der Datenauswertung")
+label_time.pack(pady=10)
+
+time_options = ["24 Stunden", "Eine Stunde", "30 Minuten", "48 Stunden"]
+time_combobox = ttk.Combobox(frame_2d, values=time_options)
+time_combobox.pack(pady=10)
+
+# Auswahl-Button, der erscheint, wenn eine Option in der Combobox ausgewählt wurde
+button_choice = tk.Button(frame_2d, text="Auswahl", command=lambda: show_frame(frame_2d_systems), bg='white', fg='black')
+def on_combobox_change(event):
+    button_choice.pack(pady=10)
+
+time_combobox.bind("<<ComboboxSelected>>", on_combobox_change)
+
+# Zurück-Button für den Frame frame_2d_systems
+back_button_2d_systems = tk.Button(frame_2d_systems, text="Zurück", command=lambda: show_frame(frame_2d), bg='white', fg='black')
+back_button_2d_systems.pack(anchor='nw')  # Positioniert den Button in der linken oberen Ecke
+
+# Zeigen Sie den Hauptframe zuerst an
+show_frame(main_frame)
 
 root.mainloop()
