dc12dded36dcf88133a7bb87d407e12ee40537a3
Harris <harris.nuhanovic@stud.fra-uas.de>
2024-01-03 21:38:47 +0100

    added second board and adjusted angles

--- main.py
+++ main.py
@@ -5,11 +5,8 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import vpython as vp
 from vpython import *
-from time import *
 import numpy as np
-import math
 import serial
-import datetime
 import json
 
 
@@ -19,6 +16,7 @@ def show_frame(frame):
     frame.tkraise()
 
 def open_scene():
+
     ser = serial.Serial('com10', 115200)
 
     # 3D-Visualisierungs-Frame
@@ -28,13 +26,12 @@ def open_scene():
 
     # Set the scene
 
-    scene = vp.canvas(frame=frame_3d, width=800, height=600)
-    scene.forward = vector(6, -1, 1)
+    scene = vp.canvas(frame=frame_3d, width=1200, height=600)
+    scene.center = vector(0, 0, 0)
+    scene.forward = vector(10, -1, 1)
     scene.background = color.white
 
-    scene2 = vp.canvas(frame=frame_3d, width=800, height=600)
-    scene2.forward = vector(6, -1, 1)
-    scene2.background = color.white
+
 
     # Create the BNO055 board
     bnoO55 = box(length=0.5, width=0.38, height=0.04, opacity=.75, pos=vec(0, -0.065, 0), color=vec(0.2, 0.2, 0.2))
@@ -93,7 +90,67 @@ def open_scene():
                       nordicboard,
                       bridge
                       ],
-                     pos=vec(0, 0, 0), origin=vec(0, 0, 0))
+                     pos=vec(0, 0, -15), origin=vec(0, 0, 0))
+
+
+    # Create the second BNO055 board
+    bnoO55_2 = box(length=0.5, width=0.38, height=0.04, opacity=.75, pos=vec(0, -0.065, 0), color=vec(0.2, 0.2, 0.2))
+    bnoO55PCB_2 = box(length=2, width=2.7, height=.13, opacity=.75, pos=vec(0, -0.13, 0), color=vec(0, 0.54, 0.69))
+    bnoO55con1_2 = box(length=0.25, width=1.2, height=1.03, opacity=.75, pos=vec(-0.85, -0.7025, 0),
+                     color=vec(0.2, 0.2, 0.2))
+    bnoO55con2_2 = box(length=0.25, width=1.6, height=1.03, opacity=.75, pos=vec(0.85, -0.7025, 0),
+                     color=vec(0.2, 0.2, 0.2))
+
+    # Combine the BNO055 components into one object
+    bnoO55board_2 = compound([bnoO55PCB_2,
+                            bnoO55_2,
+                            bnoO55con1_2,
+                            bnoO55con2_2
+                            ],
+                           pos=vec(0, 0, 0), origin=vec(0, 0, 0))
+
+    # Create the Nordic board
+    nordicdkpcb_2 = box(length=6.4, width=13.6, height=.13, opacity=.75, pos=vec(3.2, 0, 6.8), color=vec(0, 0.8, 0.5))
+    nrf52840_2 = box(length=0.7, width=0.7, height=0.04, opacity=.75, pos=vec(1.9, 0.085, 2), color=vec(0.2, 0.2, 0.2))
+
+    # Create the connectors for the Nordic board
+    nordicdkcon1_2 = box(length=.25, width=2.1, height=.7, opacity=.75, pos=vec(0.65, 0.35, 7.85), color=vec(.2, .2, .2))
+    nordicdkcon2_2 = box(length=.25, width=2.1, height=.7, opacity=.75, pos=vec(0.65, 0.35, 5.5), color=vec(.2, .2, .2))
+    nordicdkcon3_2 = box(length=.25, width=1.5, height=.7, opacity=.75, pos=vec(5.54, 0.35, 7.9), color=vec(.2, .2, .2))
+    nordicdkcon4_2 = box(length=2.3, width=0.5, height=.7, opacity=.75, pos=vec(1.9, 0.35, 4), color=vec(.2, .2, .2))
+
+    # Combine the Nordic components into one object
+    nordicboard_2 = compound([nordicdkpcb_2,
+                            nrf52840_2,
+                            nordicdkcon1_2,
+                            nordicdkcon2_2,
+                            nordicdkcon3_2,
+                            nordicdkcon4_2
+                            ],
+                           pos=vec(-3.2, -2.55, -10), origin=vec(0, 0, 0))
+
+    # Create the bridge board
+    bridgepcb1_2 = box(length=5.3, width=1.8, height=.13, opacity=.75, pos=vec(2.65, 0, 0.9), color=vec(1, 0, 0))
+    bridgepcb2_2 = box(length=1.5, width=0.9, height=.13, opacity=.75, pos=vec(0.75, 0, 2.25), color=vec(1, 0, 0))
+    bridgepcb3_2 = box(length=0.9, width=0.3, height=.13, opacity=.75, pos=vec(4.85, 0, -0.15), color=vec(1, 0, 0))
+    bridgei2ccon_2 = box(length=.25, width=2.6, height=1.2, opacity=.75, pos=vec(0.1, -0.685, 1.3), color=vec(.2, .2, .2))
+    bridgepwcon_2 = box(length=.25, width=2.1, height=1.2, opacity=.75, pos=vec(5, -0.685, 0.78), color=vec(.2, .2, .2))
+
+    # Combine the bridge components into one object
+    bridge_2 = compound([bridgepcb1_2,
+                       bridgepcb2_2,
+                       bridgepcb3_2,
+                       bridgei2ccon_2,
+                       bridgepwcon_2
+                       ],
+                      pos=vec(-2.65, -1.275, -0.9), origin=vec(0, 0, 0))
+
+    # Combine all the objects into one
+    fhObj_2 = compound([bnoO55board_2,
+                      nordicboard_2,
+                      bridge_2
+                      ],
+                     pos=vec(0, 0, 15), origin=vec(0, 0, 0))
 
     try:
         while True:
@@ -134,6 +191,8 @@ def open_scene():
 
                     unitLength = qw**2 + qx**2 + qy**2 + qz**2
                     abcd = qw*qx + qy*qz
+
+                    # added 45 for correction of angle
 
                     if(unitLength != 0):
                         if(abcd > (0.4995)*unitLength):
@@ -176,45 +235,6 @@ def open_scene():
         # Das Programm beenden, wenn Strg+C gedrückt wird
         ser.close()
         print("Programm beendet.")
-
-
-    # # Visualization
-    #
-    # while (True):
-    #     while (ser.inWaiting() == 0):
-    #         pass
-    #     try:
-    #         # Zeit:
-    #         #local_time = time.localtime()
-    #         #dt = datetime.datetime.now()
-    #         # Konvertierung der sensorwerte.
-    #         # Sensorwerte kommen in einer Reihen von Bytes an.
-    #         dataPacket = ser.readline()
-    #         dataPacket = str(dataPacket, 'utf-8')
-    #         print(dataPacket)
-    #         # Separieren der Sensor-Werte
-    #         splitPacket = dataPacket.split(",")
-    #         qw = float(splitPacket[1])  # w
-    #         qx = float(splitPacket[2])  # x
-    #         qy = float(splitPacket[3])  # y
-    #         qz = float(splitPacket[4])  # z
-    #
-    #         # Berechnungen von Winkeln
-    #         roll = -math.atan2(2 * (qw * qx + qy * qz), 1 - 2 * (qx * qx + qy * qy))
-    #         pitch = math.asin(2 * (qw * qy - qz * qx))
-    #         yaw = -math.atan2(2 * (qw * qz + qx * qy), 1 - 2 * (qy * qy + qz * qz)) - np.pi / 2
-    #         rate(50)
-    #         k = vector(cos(yaw) * cos(pitch), sin(pitch), sin(yaw) * cos(pitch))
-    #         y = vector(0, 1, 0)
-    #         s = cross(k, y)
-    #         v = cross(s, k)
-    #         vrot = v * cos(roll) + cross(k, v) * sin(roll)
-    #         # Die Winkeln an das Objekt anpassen
-    #         fhObj.axis = k
-    #         fhObj.up = vrot
-    #
-    #     except Exception as e:
-    #         print(e)
 
 
 # Erstellen Sie sechs Subplots
